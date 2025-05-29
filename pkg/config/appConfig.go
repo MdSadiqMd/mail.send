b3833823e3e22280f6b279b4a002bbadf9c6b644
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-05-29 22:17:09 +0530

    🪵  configured logger in config

--- pkg/config/appConfig.go
+++ pkg/config/appConfig.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"os"
 
+	logger "github.com/MdSadiqMd/mail.send/pkg/log"
 	"github.com/joho/godotenv"
 )
 
@@ -13,15 +14,18 @@ type AppConfig struct {
 }
 
 func SetupEnv() (cfg AppConfig, err error) {
+	config := logger.New("config")
 	godotenv.Load()
 
 	httpPort := os.Getenv("PORT")
 	if httpPort == "" {
+		config.Fatal("PORT variable not found")
 		return AppConfig{}, errors.New("PORT variable not found")
 	}
 
 	DataSourceName := os.Getenv("DB_URL")
 	if DataSourceName == "" {
+		config.Fatal("DB_URL variable not found")
 		return AppConfig{}, errors.New("DB_URL variable not found")
 	}
 
