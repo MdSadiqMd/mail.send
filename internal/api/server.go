55b0a0fcad1506e5d2a772f967b06b6fd062e955
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-05-29 22:35:24 +0530

    📦 moved routes init to main.go

--- internal/api/server.go
+++ internal/api/server.go
@@ -1,43 +1,30 @@
 package api
 
 import (
-	"fmt"
 	"net/http"
-	"time"
 
-	"github.com/MdSadiqMd/mail.send/internal/api/routes"
-	"github.com/MdSadiqMd/mail.send/pkg/config"
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	_ "github.com/joho/godotenv/autoload"
 	"gorm.io/gorm"
 )
 
 type Server struct {
-	port string
-	db   *gorm.DB
+	db *gorm.DB
 }
 
-func NewServer(config config.AppConfig, db *gorm.DB) *http.Server {
-	newServer := &Server{
-		port: config.ServerPort,
-		db:   db,
+func NewServer(db *gorm.DB) *Server {
+	return &Server{
+		db: db,
 	}
-
-	server := &http.Server{
-		Addr:         fmt.Sprintf(":%s", newServer.port),
-		Handler:      newServer.RegisterRoutes(),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 30 * time.Second,
-	}
-	return server
 }
 
-func (s *Server) RegisterRoutes() http.Handler {
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
-	routes.RegisterHealthRoutes(r, s.db)
-	return r
+func (s *Server) RegisterRoutes(r chi.Router) {
+	s.registerHealthRoutes(r)
+}
+
+func (s *Server) registerHealthRoutes(r chi.Router) {
+	r.Route("/health", func(r chi.Router) {
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("OK"))
+		})
+	})
 }
