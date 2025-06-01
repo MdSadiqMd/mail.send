bd362caeb3be4971677e79f4afe6b08afe3424f3
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-06-01 16:15:49 +0530

    🪵  configured logger in randomstrings

--- pkg/utils/randomStrings.go
+++ pkg/utils/randomStrings.go
@@ -1,14 +1,19 @@
-package randomstrings
+package utils
 
 import (
 	"crypto/rand"
 	"encoding/base64"
+
+	logger "github.com/MdSadiqMd/mail.send/pkg/log"
 )
+
+var helperLogger = logger.New("RandomStrings")
 
 func GenerateRandomBytes(length int) ([]byte, error) {
 	bytes := make([]byte, length)
 	_, err := rand.Read(bytes)
 	if err != nil {
+		helperLogger.Error("Failed to generate random bytes: %v", err)
 		return nil, err
 	}
 	return bytes, nil
@@ -16,5 +21,9 @@ func GenerateRandomBytes(length int) ([]byte, error) {
 
 func GenerateRandomString(s int) (string, error) {
 	bytes, err := GenerateRandomBytes(s)
-	return base64.URLEncoding.EncodeToString(bytes), err
+	if err != nil {
+		helperLogger.Error("Failed to generate random string: %v", err)
+		return "", err
+	}
+	return base64.URLEncoding.EncodeToString(bytes), nil
 }
