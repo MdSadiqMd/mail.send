09818a44169ba8739371dcfeed748309bd334240
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-06-07 22:37:12 +0530

    ✅ added IsVerified feild

--- internal/models/user.go
+++ internal/models/user.go
@@ -5,13 +5,14 @@ import (
 )
 
 type User struct {
-	Id        uint      `json:"id" gorm:"primary_key" unique:"true"`
-	FirstName string    `json:"first_name"`
-	LastName  string    `json:"last_name"`
-	Email     string    `json:"email" gorm:"index;unique;not null"`
-	Password  string    `json:"password" gorm:"not null"`
-	Role      string    `json:"role" gorm:"not null"`
-	CreatedAt time.Time `json:"created_at" gorm:"default:current_timestamp"`
-	UpdatedAt time.Time `json:"updated_at" gorm:"default:current_timestamp"`
-	VoidedOn  time.Time `json:"voided_on" gorm:"default:NULL"`
+	Id         uint      `json:"id" gorm:"primary_key" unique:"true"`
+	FirstName  string    `json:"first_name"`
+	LastName   string    `json:"last_name"`
+	Email      string    `json:"email" gorm:"index;unique;not null"`
+	Password   string    `json:"password" gorm:"not null"`
+	Role       string    `json:"role" gorm:"not null"`
+	IsVerified bool      `json:"isVerified" gorm:"not null"`
+	CreatedAt  time.Time `json:"created_at" gorm:"default:current_timestamp"`
+	UpdatedAt  time.Time `json:"updated_at" gorm:"default:current_timestamp"`
+	VoidedOn   time.Time `json:"voided_on" gorm:"default:NULL"`
 }
