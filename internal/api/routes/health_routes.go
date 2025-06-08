b97522a02d292e748afbb8126e4dd21c0268975c
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-06-08 20:16:18 +0530

    ❤️‍🩹 migrated health routes to chi from net/http

--- internal/api/routes/health_routes.go
+++ internal/api/routes/health_routes.go
@@ -3,12 +3,13 @@ package routes
 import (
 	"net/http"
 
+	"github.com/MdSadiqMd/mail.send/pkg/config"
 	"github.com/go-chi/chi/v5"
-	"gorm.io/gorm"
 )
 
-func RegisterHealthRoutes(r chi.Router, db *gorm.DB) {
-	r.Route("/", func(r chi.Router) {
+func RegisterHealthRoutes(handler config.Handler) {
+	app := handler.App
+	app.Route("/", func(r chi.Router) {
 		r.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
 			w.Write([]byte("OK"))
 		})
